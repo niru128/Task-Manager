9cedd4bf4b6b1997be3fbee278eeaeb0a917f3a7
Niranjan C B <niranjancb2035@gmail.com>
2026-05-30 17:14:23 +0530

    Added models,controller functionality in the backend

---
+++ backend/models/Task.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    stage: {
+      type: String,
+      enum: ["Todo", "In Progress", "Done"],
+      default: "Todo",
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
