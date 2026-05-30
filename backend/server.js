580ad064224082a63f48ddd3494edf0c7e0c3368
Niranjan C B <niranjancb2035@gmail.com>
2026-05-30 17:45:50 +0530

    Updated environment variables for backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
@@ -12,7 +11,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
